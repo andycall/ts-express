697ec21d9f8d2f26af9987524c99032252fa7760
andycall <dongtiancheng@baidu.com>
2017-06-12 18:50:31 +0800

    add response setHeader and getHeader

--- src/response.ts
+++ src/response.ts
@@ -1,6 +1,7 @@
 /// <reference types="node" />
 
 import { ServerResponse, IncomingMessage } from 'http'
+import {mime} from 'send'
 
 export declare type responseBody = string | number | boolean | Buffer;
 
@@ -8,6 +9,8 @@ export interface ExpressResponse extends ServerResponse {
     status(code: number): ExpressResponse;
     send(body: responseBody): ExpressResponse;
 }
+
+const charsetRegExp = /;\s*charset\s*=/;
 
 export class ExpressResponse implements ExpressResponse {
     public _req: IncomingMessage;
@@ -41,7 +44,35 @@ export class ExpressResponse implements ExpressResponse {
         return this;
     }
 
+    header(field: string, val: string) {
+        return this.set(field, val);
+    }
+
+    set(field: string, val: string) {
+        if (field.toLowerCase() === 'content-type') {
+            if (!charsetRegExp.test(val)) {
+                let charset = mime.charsets.lookup(val.split(';')[0]);
+                if (charset) {
+                    val += `; charset=${charset.toLowerCase()}`;
+                }
+            }
+        }
+
+        this.setHeader(field, val);
+        return this;
+    }
+
+    get(field: string) {
+        return this.getHeader(field);
+    }
+
+    type(type: string) {
+
+    };
+
     send(body: responseBody) {
+        let chunk = body;
+
         return this;
     }
 }
