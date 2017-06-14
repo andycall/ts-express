9081e514930b9a9630b23e303d067c2cd7373d0c
andycall <dongtiancheng@baidu.com>
2017-06-14 22:28:40 +0800

    add more tests

--- test/res.redirect.test.ts
+++ test/res.redirect.test.ts
@@ -30,6 +30,36 @@ describe("res",  () => {
                 .get('/')
                 .expect('Location', 'https://google.com?q=%E2%98%83%20%C2%A710')
                 .expect(302, done)
+        });
+
+        it('should not touch already-encoded sequences in "url"', done => {
+            const app = express();
+            const router = app.router;
+
+            router.use((req, res) => {
+                res.redirect('https://google.com?q=%A710');
+            });
+
+            supertest(app.requestHandle)
+                .get('/')
+                .expect('Location', 'https://google.com?q=%A710')
+                .expect(302, done);
+        })
+    });
+
+    describe('.redirect(url, status)', () => {
+        it('should set the response status', done => {
+            const app = express()
+            const router = app.router;
+
+            router.use((req, res) => {
+                res.redirect('https://google.com', 301);
+            });
+
+            supertest(app.requestHandle)
+                .get('/')
+                .expect('Location', 'https://google.com')
+                .expect(301, done);
         })
     })
 });
